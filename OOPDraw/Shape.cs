ad29be6d1dbee120aaf4657c6f09ee951e4bf00c
Max-Miller-10 <millermg@stpaulsschool.org.uk>
2026-02-05 16:19:12 +0000

    Started code

---
+++ OOPDraw/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace OOPDraw
+{
+    public abstract class Shape
+    {
+        public abstract void Draw(Graphics g);
+        public abstract void GrowTo(int x2, int y2);
+
+    }
+}
